378b6a000cfb7520a5b9286966556086b9e8f912
ShieldPro <shieldpro@wp.pl>
2022-04-10 17:12:26 +0200

    CosmeticStore

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>OyasumeCracker</title>
+
+		<meta name="description" content="Darmowe kosmetyki do twojego klienta mincecraft.">
+		<meta name="keywords" content="Darmowe kosmetyki, darmowe, kosmetyki, minecraft, lunar, client, blazingpack, mojang, badlion">
+
+		<meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
+
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body class="bg-dark">
+		<nav class="navbar navbar-dark bg-gray" style="background-color: rgb(29, 32, 36);">
+ 			<div class="container-fluid">
+ 			  <a class="navbar-brand" href="#">
+				<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-activity d-inline-block align-text-top gradientColor" viewBox="0 0 16 16" id="logo">
+  					<path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
+				</svg>
+ 			   	<h style="padding-left: 5px">Cosmetic Store</h>
+ 			  </a>
+ 			</div>
+		</nav>
+
+		<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+			<div class="carousel-indicators ">
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+			</div>
+
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<div class="d-block w-100" style='height: calc(100vh - 60px); background-image:linear-gradient(to top, rgba(0,0,0,0.75),rgba(0,0,0,0.1)), url("img/lunarbg.png"); background-position: center; background-size: cover;' alt="...">
+						<img src="img/lunarlogo.png" class="mx-auto d-block" style="position: relative; top: 30%; height: 150px;"/>
+						<p class="text-light text-center" style="position: relative; top: 30%; font-size: 50px;"><b>Lunar Client</b></p>
+						<button type="button" class="btn btn-outline-light mx-auto d-block" style="position: relative; top: 30%">PRZEJDŹ DO KOSMETYKÓW</button>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<div class="d-block w-100" style='height: calc(100vh - 60px); background-image:linear-gradient(to top, rgba(0,0,0,0.75),rgba(0,0,0,0.1)), url("img/featherbg.png"); background-position: center; background-size: cover;' alt="...">
+						<img src="img/featherlogo.png" class="mx-auto d-block" style="position: relative; top: 30%; height: 150px;"/>
+						<p class="text-light text-center" style="position: relative; top: 30%; font-size: 50px;"><b>Feather Client</b></p>
+						<button type="button" class="btn btn-outline-light mx-auto d-block" style="position: relative; top: 30%">PRZEJDŹ DO KOSMETYKÓW</button>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<div class="d-block w-100" style='height: calc(100vh - 60px); background-image:linear-gradient(to top, rgba(0,0,0,0.75),rgba(0,0,0,0.1)), url("img/badlionbg.jpg"); background-position: center; background-size: cover;' alt="...">
+						<img src="img/badlionlogo.png" class="mx-auto d-block" style="position: relative; top: 30%; height: 150px;"/>
+						<p class="text-center" style="position: relative; top: 30%; font-size: 50px; color: #feb730;"><b>Badlion</b></p>
+						<button type="button" class="btn btn-outline-light mx-auto d-block" style="position: relative; top: 30%;">PRZEJDŹ DO KOSMETYKÓW</button>
+					</div>
+				</div>
+			</div>
+
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+	</body>
+</html>
